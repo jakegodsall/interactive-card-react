5c70569d2964eb31dbe10e0ff203c230000aacdc
jakegodsall <jake.edward.godsall@gmail.com>
2022-09-04 12:03:30 +0100

    added state to BankForm for all inputs, provided onChange events and made inputs controlled components

--- src/components/BankForm/BankForm.js
+++ src/components/BankForm/BankForm.js
@@ -1,38 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../UI/Button/Button';
 
 import './BankForm.css';
 
 const BankForm = props => {
 
+    const [holderName, setHolderName] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [expMonth, setExpMonth] = useState('');
+    const [expYear, setExpYear] = useState('');
+    const [cvc, setCvc] = useState('');
+
+    const holderNameHandler = e => {
+        setHolderName(e.target.value);
+    };
+
+    const cardNumberHandler = e => {
+        setCardNumber(e.target.value);
+    };
+
+    const expMonthHandler = e => {
+        setExpMonth(e.target.value);
+    };
+
+    const expYearHandler = e => {
+        setExpYear(e.target.value);
+    };
+
+    const cvcHandler = e => {
+        setCvc(e.target.value);
+    };
+
     const formHandler = e => {
         e.preventDefault();
-    }
+
+        // create user data object
+        const userData = {
+            name: holderName,
+            cardNumber: cardNumber,
+            expMonth: expMonth,
+            expYear: expYear,
+            cvc: cvc,
+            id: Math.random().toString()
+        };
+
+        console.log(userData);
+
+        // reset form values
+        setHolderName('');
+        setCardNumber('');
+        setExpMonth('');
+        setExpYear('');
+        setCvc('');
+    };
 
     return (
         <form className="bank-form" onSubmit={formHandler}>
             <div className="bank-form__row">
                 <label className="bank-form__label">cardholder name</label>
-                <input type="text" />
+                <input className="bank-form__input" type="text" value={holderName} onChange={holderNameHandler} />
             </div>
             <div className="bank-form__row">
                 <label className="bank-form__label">card number</label>
-                <input type="number" />
+                <input className="bank-form__input" type="number" value={cardNumber} onChange={cardNumberHandler} />
             </div>
-            <div className="bank-form__row-final">
+            <div className="bank-form__row final">
                 <div className="bank-form__col1">
                     <label className="bank-form__label">exp. date (mm/yy)</label>
-                    <div>
-                        <input type="number" />
-                        <input type="number" />
+                    <div className="bank-form__exp-date">
+                        <input className="bank-form__input" type="number" value={expMonth} onChange={expMonthHandler} />
+                        <input className="bank-form__input" type="number" value={expYear} onChange={expYearHandler} />
                     </div>
                 </div>
                 <div className="bank-form__col2">
                     <label className="bank-form__label">cvc</label>
-                    <input type="number" />
+                    <input className="bank-form__input" type="number" value={cvc} onChange={cvcHandler} />
                 </div>
-                
-                
             </div>
             <Button />
         </form>
