45e311347f6af2bdbadbf7125602375a4567039b
jakegodsall <jake.edward.godsall@gmail.com>
2022-09-07 11:13:41 +0100

    created list of objects representing input values and used it to generate UI

--- src/components/BankForm/BankForm.js
+++ src/components/BankForm/BankForm.js
@@ -1,124 +1,76 @@
 import React, { useState } from 'react';
+
+import FormInput from './FormInput';
 import Button from '../UI/Button/Button';
 
 import './BankForm.css';
 
 const BankForm = props => {
-	// input states
-	const [holderName, setHolderName] = useState('');
-	const [cardNumber, setCardNumber] = useState('');
-	const [expMonth, setExpMonth] = useState('');
-	const [expYear, setExpYear] = useState('');
-	const [cvc, setCvc] = useState('');
+	const [values, setValues] = useState({
+		name: '',
+		cardNumber: '',
+		expMonth: '',
+		expYear: '',
+		cvc: '',
+	});
 
-	// error states
-	const [holderNameError, setHolderNameError] = useState('');
-	const [cardNumberError, setCardNumberError] = useState('');
-	const [expMonthError, setExpMonthError] = useState('');
-	const [expYearError, setExpYearError] = useState('');
-	const [cvcError, setCvcError] = useState('');
-
-	const onlyNumbers = value => {
-		const valuesArray = value.split('');
-		const nums = '0123456789';
-
-		valuesArray.forEach(el => {
-			if (!nums.includes(el)) {
-				return false;
-			}
-		});
-		return true;
-	};
-
-	const holderNameHandler = e => {
-		setHolderName(e.target.value);
-	};
-
-	const cardNumberHandler = e => {
-		const value = e.target.value;
-
-		setCardNumber(value);
-
-		// reset error message
-		if (value.length === 0) {
-			setCardNumberError('');
-		}
-
-		// validate every character is a number
-		if (onlyNumbers(value)) {
-			setCardNumberError('Wrong format, numbers only');
-		}
-	};
-
-	const expMonthHandler = e => {
-		setExpMonth(e.target.value);
-	};
-
-	const expYearHandler = e => {
-		setExpYear(e.target.value);
-	};
-
-	const cvcHandler = e => {
-		const value = e.target.value;
-		setCvc(e.target.value);
-
-		console.log(cvc);
-
-		// reset error message
-		if (value.length === 0) {
-			setCvcError('');
-		}
-	};
+	const inputs = [
+		{
+			id: 1,
+			name: 'name',
+			label: 'cardholder name',
+			placeholder: 'e.g. Jane Appleseed',
+			type: 'text',
+			errorMessage:
+				'Please enter your full name (first name and surname separated by a space)',
+			required: true,
+		},
+		{
+			id: 2,
+			name: 'cardNumber',
+			label: 'card number',
+			placeholder: 'e.g. 1234 5678 9123 0000',
+			type: 'number',
+			errorMessage: 'Wrong format, numbers only',
+			required: true,
+		},
+		{
+			id: 3,
+			name: 'expMonth',
+			label: 'exp. date',
+			placeholder: 'MM',
+			type: 'number',
+			errorMessage: 'Must be a month in the range 00-12',
+			required: true,
+		},
+		{
+			id: 4,
+			name: 'expYear',
+			label: '(mm/yy)',
+			placeholder: 'YY',
+			type: 'number',
+			errorMessage: 'Must be a year in the range (00-22)',
+			required: true,
+		},
+		{
+			id: 5,
+			name: 'cvc',
+			label: 'cvc',
+			placeholder: 'e.g. 123',
+			type: 'number',
+			errorMessage: 'Enter a valid CVC (3 digits)',
+			required: true,
+		},
+	];
 
 	const submitHandler = e => {
 		e.preventDefault();
+	};
 
-		// name onsubmit validation
-		if (holderName.length === 0) {
-			setHolderNameError("Can't be blank");
-		} else if (holderName.split(' ').length !== 2) {
-			setHolderNameError('Please enter your forename and surname');
-		}
-
-		// card number onsubmit validation
-		if (cardNumber.length === 0) {
-			setCardNumberError("Can't be blank");
-		} else if (cardNumber.length < 16 || cardNumber.length > 18) {
-			setCardNumberError('Card number is not the correct length');
-		} else {
-			setCardNumberError('');
-		}
-
-		// expiry month onsubmit validation
-		if (expMonthError.length === 0) {
-			setExpMonthError("Can't be blank");
-		}
-
-		// expiry year onsubmit validation
-		if (expYearError.length === 0) {
-			setExpYearError("Can't be blank");
-		}
-
-		if (cvc.length === 0) {
-			// cvc on submitvalidation
-			setCvcError("Can't be blank");
-		} else if (cvc.length !== 3) {
-			setCvcError('Must be exactly three digits');
-		} else {
-			setCvcError('');
-		}
-
-		// create user data object
-		const userData = {
-			name: holderName,
-			cardNumber: cardNumber,
-			expMonth: expMonth,
-			expYear: expYear,
-			cvc: cvc,
-			id: Math.random().toString(),
-		};
-
-		console.log(userData);
+	const onChange = e => {
+		console.log(e.target.value, e.target.name);
+		setValues({ ...values, [e.target.name]: e.target.value });
+		console.log(values);
 	};
 
 	return (
@@ -126,59 +78,76 @@ const BankForm = props => {
 			className='bank-form'
 			onSubmit={submitHandler}
 		>
-			<div className='bank-form__row'>
-				<label className='bank-form__label'>cardholder name</label>
-				<input
-					className='bank-form__input'
-					type='text'
-					value={holderName}
-					onChange={holderNameHandler}
+			<FormInput
+				className='bank-form__row'
+				key={inputs[0].id}
+				name={inputs[0].name}
+				label={inputs[0].label}
+				type={inputs[0].type}
+				placeholder={inputs[0].placeholder}
+				errorMessage={inputs[0].errorMessage}
+				required={inputs[0].required}
+				value={values[inputs[0].name]}
+				onChange={onChange}
+			/>
+
+			<FormInput
+				className='bank-form__row'
+				key={inputs[1].id}
+				name={inputs[1].name}
+				label={inputs[1].label}
+				type={inputs[1].type}
+				placeholder={inputs[1].placeholder}
+				errorMessage={inputs[1].errorMessage}
+				required={inputs[1].required}
+				value={values[inputs[1].name]}
+				onChange={onChange}
+			/>
+
+			<div className='bank-form__final-row'>
+				<FormInput
+					className='bank-form__row'
+					inputClassName='bank-form__exp-date'
+					key={inputs[2].id}
+					name={inputs[2].name}
+					label={inputs[2].label}
+					type={inputs[2].type}
+					placeholder={inputs[2].placeholder}
+					errorMessage={inputs[2].errorMessage}
+					required={inputs[2].required}
+					value={values[inputs[2].name]}
+					onChange={onChange}
 				/>
-				{<p>{holderNameError}</p>}
-			</div>
-			<div className='bank-form__row'>
-				<label className='bank-form__label'>card number</label>
-				<input
-					className='bank-form__input'
-					type='text'
-					value={cardNumber}
-					onChange={cardNumberHandler}
+
+				<FormInput
+					className='bank-form__row'
+					inputClassName='bank-form__exp-date'
+					key={inputs[3].id}
+					name={inputs[3].name}
+					label={inputs[3].label}
+					type={inputs[3].type}
+					placeholder={inputs[3].placeholder}
+					errorMessage={inputs[3].errorMessage}
+					required={inputs[3].required}
+					value={values[inputs[3].name]}
+					onChange={onChange}
 				/>
-				{<p>{cardNumberError}</p>}
+
+				<FormInput
+					className='bank-form__row'
+					inputClassName='bank-form__cvc'
+					key={inputs[4].id}
+					name={inputs[4].name}
+					label={inputs[4].label}
+					type={inputs[4].type}
+					placeholder={inputs[4].placeholder}
+					errorMessage={inputs[4].errorMessage}
+					required={inputs[4].required}
+					value={values[inputs[4].name]}
+					onChange={onChange}
+				/>
 			</div>
-			<div className='bank-form__row final'>
-				<div className='bank-form__col1'>
-					<label className='bank-form__label'>
-						exp. date (mm/yy)
-					</label>
-					<div className='bank-form__exp-date'>
-						<input
-							className='bank-form__input'
-							type='number'
-							value={expMonth}
-							onChange={expMonthHandler}
-						/>
-						{<p>{expMonthError}</p>}
-						<input
-							className='bank-form__input'
-							type='number'
-							value={expYear}
-							onChange={expYearHandler}
-						/>
-						{<p>{expYearError}</p>}
-					</div>
-				</div>
-				<div className='bank-form__col2'>
-					<label className='bank-form__label'>cvc</label>
-					<input
-						className='bank-form__input'
-						type='number'
-						value={cvc}
-						onChange={cvcHandler}
-					/>
-					{<p>{cvcError}</p>}
-				</div>
-			</div>
+
 			<Button />
 		</form>
 	);
